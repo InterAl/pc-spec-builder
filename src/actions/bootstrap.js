a1d7d2c14213de814955bafc340e3eb3a67c031d
Alon Nachshon <alonn@wix.com>
2016-07-17 00:38:45 +0300

    system options in a separate file + all nuc systems

--- src/actions/bootstrap.js
+++ src/actions/bootstrap.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import Q from 'q';
 import 'whatwg-fetch';
 import {setSpecOptions} from '../actions/setSpecOptions';
-import specOptions from '../specOptions';
+import systemOptions from '../systemOptions';
 import tsv from 'tsv';
 
 export default function() {
@@ -61,63 +61,6 @@ function plonterFileToSpecOptions(plonter) {
         }
     }
 
-    function getSystems() {
-        return [{
-            id: 0,
-            name: 'Maestro AiO',
-            tags: ['0MAESTRO', 'DSSD', 'CHDD35', 'ACLV1050', 'CDDR3M', 'zzsoft', 'zzzservice', 'ZUPS']
-        }, {
-            id: 1,
-            name: 'NUC',
-            tags: ['06NUC', 'CHDD25', 'DDR4SODIM', 'DSSD25', 'DSSDM2SATA', 'zzsoft', 'zzzservice', 'ZUPS', 'ZNUCCABLES']
-        }, {
-            id: 2,
-            name: 'NAS Builder',
-            tags: ['0NAS', 'CHDD35NAS', 'DSSENTD25']
-        }, {
-            id: 3,
-            name: 'Intel 6th Generation LGA1151 Skylake Series DDR4',
-            subsystems: [{
-                name: 'ATX',
-                tags: ['AC1151', 'B11514ATX', 'B11514mATX', 'CDDR4D', 'CDISPLAY', 'ZLCD', 'EATXC', 'ZFANS', 'EATXPSU', 'ZKYBD', 'ZMOUSE', 'ZSOUND', 'ZVC', 'ZREMOTE', 'ZOPTICAL', 'ZMICEAR', 'DSSD', 'zzsoft', 'zzzservice', 'ZUPS', 'CHDD35', 'ZLAN', 'NWIFI', 'ZSTICK']
-            }, {
-                name: 'ITX',
-                tags: ['AC1151', 'ACLV11514', 'B11514ITX', 'CDDR4D', 'CDISPLAY', 'ZLCD', 'EITXC', 'ZFANS', 'EATXPSU', 'ZKYBD', 'ZMOUSE', 'ZSOUND', 'ZVC', 'ZREMOTE', 'ZOPTICAL', 'ZMICEAR', 'DSSD', 'zzsoft', 'zzzservice', 'ZUPS', 'CHDD', 'ZLAN', 'NWIFI', 'ZSTICK']
-            }, {
-                name: 'HTPC',
-                tags: ['AC1151', 'ACLV11514', 'B11514ITX', 'B11514ATX', 'B11514mATX', 'CDDR4D', 'CDISPLAY', 'ZLCD', 'EHTPC', 'ZKYBD', 'ZMOUSE', 'ZSOUND', 'ZVC', 'ZREMOTE', 'ZOPTICAL', 'ZMICEAR', 'DSSD', 'zzsoft', 'zzzservice', 'ZUPS', 'CHDD35', 'ZLAN', 'NWIFI', 'ZFANS', 'ZSTICK']
-            }]
-        }, {
-            id: 4,
-            name: 'Intel 4th/5th Generation LGA1150 Haswell/Broadwell Series',
-            subsystems: [{
-                name: 'ATX',
-                tags: ['AC1050', 'B1150ATX', 'B1150mATX', 'CDDR3D', 'CDISPLAY', 'ZLCD', 'EATXC', 'ZFANS', 'EATXPSU', 'ZKYBD', 'ZMOUSE', 'ZSOUND', 'ZVC', 'ZREMOTE', 'ZOPTICAL', 'ZMICEAR', 'DSSD', 'zzsoft', 'zzzservice', 'ZUPS', 'CHDD35', 'ZLAN', 'NWIFI', 'ZSTICK']
-            }, {
-                name: 'ITX',
-                tags: ['AC1050', 'ACLV1050', 'B1150ITX', 'CDDR3D', 'CDISPLAY', 'ZLCD', 'EITXC', 'ZFANS', 'EATXPSU', 'ZKYBD', 'ZMOUSE', 'ZSOUND', 'ZVC', 'ZREMOTE', 'ZOPTICAL', 'ZMICEAR', 'DSSD', 'zzsoft', 'zzzservice', 'ZUPS', 'CHDD', 'ZLAN', 'NWIFI', 'ZSTICK']
-            }, {
-                name: 'HTPC',
-                tags: ['AC1050', 'ACLV1050', 'B1150ATX', 'B1150mATX', 'CDDR3D', 'CDISPLAY', 'ZLCD', 'EHTPC', 'ZKYBD', 'ZMOUSE', 'ZSOUND', 'ZVC', 'ZREMOTE', 'ZOPTICAL', 'ZMICEAR', 'DSSD', 'zzsoft', 'zzzservice', 'ZUPS', 'CHDD35', 'ZLAN', 'NWIFI', 'HTPC', 'ZFANS', 'ZSTICK']
-            }]
-        }, {
-            id: 5,
-            name: 'Intel LGA2011-3 Haswell-E Series X99 Chipset/DDR4',
-            subsystems: [{
-                name: 'ATX',
-                tags: ['AC20113', 'CDDR4D', 'B20113ATX', 'EATXC', 'ZFANS', 'EATXPSU', 'CDISPLAY', 'ZLCD', 'ZKYBD', 'ZMOUSE', 'ZSOUND', 'ZVC', 'ZREMOTE', 'ZOPTICAL', 'ZMICEAR', 'DSSD', 'zzsoft', 'zzzservice', 'ZUPS', 'CHDD35', 'ZLAN', 'NWIFI', 'ZSTICK']
-            }]
-        }, {
-            id: 6,
-            name: 'all Intel - PRO Users',
-            tags: ['AC20113', '1151', 'CDDR4D', 'B20113ATX', '1050', '1150', '2011', 'CDISPLAY', 'ZLCD', 'ZKYBD', 'ZMOUSE', 'ZSOUND', 'ZVC', 'ZREMOTE', 'ZOPTICAL', 'ZMICEAR', 'DSSD', 'DDR3', 'ATX', 'ZFANS', 'ITX', 'EMBD', 'zzsoft', 'zzzservice', 'ZUPS', 'CHDD', 'ZLAN', 'NWIFI', 'ZSTICK']
-        }, {
-            id: 7,
-            name: 'ITX Embedded systems Intel/AMD CPUs',
-            tags: ['EMBD', 'DDR3', 'ITXC', 'CDISPLAY', 'ZLCD', 'ZKYBD', 'ZMOUSE', 'ZSOUND', 'ZVC', 'ZREMOTE', 'ZOPTICAL', 'ZMICEAR', 'DSSD', 'zzsoft', 'zzzservice', 'ZUPS', 'CHDD', 'ZLAN', 'NWIFI', 'ZSTICK']
-        }];
-    }
-
     for (let i=0; i < plonter.length; i++) {
         let line = plonter[i];
         let categoryId = extractField(line, 'division', categories);
@@ -134,17 +77,14 @@ function plonterFileToSpecOptions(plonter) {
             division: line.category,
             tags
         });
-
-        if (line.title && line.title.indexOf('Windows8.1') === 0)
-            console.log(products[products.length - 1]);
     }
 
-    let systems = getSystems();
-    _.each(systems, s => {
+    _.each(systemOptions, s => {
         if (s.tags)
             s.tags = _.map(s.tags, t => t.toLowerCase());
         _.each(s.subsystems, sub => sub.tags = _.map(sub.tags, t => t.toLowerCase()));
     });
-    let result = {products, categories, tags, systems};
+
+    let result = {products, categories, tags, systems: systemOptions};
     return result;
 }
